c8e2b8269515d739addcdcf8c8a8db6685101d6b
Yi Zu <zuyi@bu.edu>
2022-11-28 13:02:27 -0500

    ICanvas interface

---
+++ HW8/src/model/ICanvas.java
@@ -0,0 +1,23 @@
+package model;
+
+import java.awt.Color;
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+
+public interface ICanvas {
+  public String getId(); // Get canvasId
+
+  public ArrayList<IShape> getShapeList();
+
+  public IShape getShape(String shapeId) throws IllegalArgumentException;
+
+  public IShape createShape(IShape shape);
+  public String toString();
+
+  public IShape move(IShape shape, Point2D targetPosition);
+
+  public IShape changeColor(IShape shape, Color targetColor);
+
+  public IShape scale(IShape shape, double targetWidth, double targetHeight);
+
+}
