690eb2747230150e80fb93fa201df138b56a3a01
Yi Zu <zuyi@bu.edu>
2022-11-29 13:04:21 -0500

    Update canvas interface

--- HW8/src/model/ICanvas.java
+++ HW8/src/model/ICanvas.java
@@ -5,14 +5,20 @@ import java.awt.geom.Point2D;
 import java.util.ArrayList;
 
 public interface ICanvas {
-  public String getId(); // Get canvasId
+
+  // Get canvasId
+  public String getId();
 
   public ArrayList<IShape> getShapeList();
 
   public IShape getShape(String shapeId) throws IllegalArgumentException;
 
-  public IShape createShape(IShape shape) throws IllegalArgumentException;;
-  public String toString();
+  public IShape createShape(String id, String name, Point2D position,
+                            double dimension1, double position2, Color color, String shapeType) throws IllegalArgumentException;
+
+  public void removeCanvas();
+
+  public void removeShape(String id);
 
   public IShape move(IShape shape, Point2D targetPosition)
           throws IllegalArgumentException;
@@ -22,5 +28,10 @@ public interface ICanvas {
 
   public IShape scale(IShape shape, double targetWidth, double targetHeight)
           throws IllegalArgumentException;
+
+  public String toString();
+
+
+
 
 }
