ac97afea345a80676f8469cc3c7770aa6547a226
Yi Zu <zuyi@bu.edu>
2022-11-30 12:47:13 -0500

    Add javadoc

--- HW8/src/model/CanvasImpl.java
+++ HW8/src/model/CanvasImpl.java
@@ -5,10 +5,16 @@ import java.awt.geom.Point2D;
 import java.util.ArrayList;
 import java.util.Objects;
 
+/**
+ * Canvas implementation.
+ */
 public class CanvasImpl implements ICanvas {
   private String id;
   private ArrayList<IShape> shapeList;
 
+  /**
+   * Canvas implementation representing instance before deciding to take a snapshot.
+   */
   public CanvasImpl(String id) throws IllegalArgumentException {
     if (id == null || id.equals("")) {
       throw new IllegalArgumentException("Id cannot be null or empty string.");
@@ -17,16 +23,31 @@ public class CanvasImpl implements ICanvas {
     this.shapeList = new ArrayList<IShape>();
   }
 
+  /**
+   * Get id of canvas.
+   *
+   * @return String id
+   */
   @Override
   public String getId() {
     return this.id;
   }
 
+  /**
+   * Get id the list of shapes.
+   *
+   * @return ArrayList<IShape>
+   */
   @Override
   public ArrayList<IShape> getShapeList() {
     return this.shapeList;
   }
 
+  /**
+   * Get shape.
+   *
+   * @return IShape
+   */
   @Override
   public IShape getShape(String shapeId) {
 
@@ -43,6 +64,11 @@ public class CanvasImpl implements ICanvas {
     return result;
   }
 
+  /**
+   * Create a shape.
+   *
+   * @return IShape
+   */
   @Override
   public IShape createShape(String id, String name, Point2D position,
                             double dimension1, double dimension2, Color color, String shapeType) throws IllegalArgumentException {
@@ -58,17 +84,29 @@ public class CanvasImpl implements ICanvas {
     }
   }
 
+  /**
+   * Moves a shape to a target position.
+   *
+   */
   @Override
   public void move(IShape shape, Point2D targetPosition)
           throws IllegalArgumentException {
   }
 
+  /**
+   * Change color a shape.
+   *
+   */
   @Override
   public void changeColor(IShape shape, Color targetColor)
           throws IllegalArgumentException {
     shape.setColor(targetColor);
   }
 
+  /**
+   * Scale a shape to bigger or smaller size.
+   *
+   */
   @Override
   public void scale(IShape shape, double targetWidth, double targetHeight)
           throws IllegalArgumentException {
@@ -77,10 +115,19 @@ public class CanvasImpl implements ICanvas {
     shape.setDimension2(targetHeight);
   }
 
+  /**
+   * Remove canvas.
+   *
+   */
   @Override
   public void removeCanvas() {
     this.shapeList = new ArrayList<IShape>();
   }
+
+  /**
+   * Remove a shape from canvas.
+   *
+   */
   @Override
   public void removeShape(String id) {
     this.shapeList.remove(getShape(id));
--- HW8/src/model/ICanvas.java
+++ HW8/src/model/ICanvas.java
@@ -4,31 +4,77 @@ import java.awt.Color;
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
 
+/**
+ * Interface for a canvas.
+ */
 public interface ICanvas {
 
-  // Get canvasId
+  /**
+   * Get id of canvas.
+   *
+   * @return String id
+   */
   public String getId();
 
+  /**
+   * Get id the list of shapes.
+   *
+   * @return ArrayList<IShape>
+   */
   public ArrayList<IShape> getShapeList();
 
+  /**
+   * Get shape.
+   *
+   * @return IShape
+   */
   public IShape getShape(String shapeId) throws IllegalArgumentException;
 
+  /**
+   * Create a shape.
+   *
+   * @return IShape
+   */
   public IShape createShape(String id, String name, Point2D position,
                             double dimension1, double position2, Color color, String shapeType) throws IllegalArgumentException;
-
+  /**
+   * Remove canvas.
+   *
+   */
   public void removeCanvas();
 
+  /**
+   * Remove a shape from canvas.
+   *
+   */
   public void removeShape(String id);
 
+  /**
+   * Moves a shape to a target position.
+   *
+   */
   public void move(IShape shape, Point2D targetPosition)
           throws IllegalArgumentException;
 
+  /**
+   * Change color a shape.
+   *
+   */
   public void changeColor(IShape shape, Color targetColor)
-          throws IllegalArgumentException;;
+          throws IllegalArgumentException;
 
+  /**
+   * Scale a shape to bigger or smaller size.
+   *
+   */
   public void scale(IShape shape, double targetWidth, double targetHeight)
           throws IllegalArgumentException;
 
+  /**
+   * Print out string version of canvas with its values.
+   *
+   * @return String
+   */
   public String toString();
 
 
