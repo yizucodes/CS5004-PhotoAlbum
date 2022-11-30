4932cf7c0cd1b977f9d2011a225b9d0de4b21ba1
Yi Zu <zuyi@bu.edu>
2022-11-29 21:15:48 -0500

    Interface for snapshot

---
+++ HW8/src/model/ISnapshot.java
@@ -0,0 +1,41 @@
+package model;
+
+import java.sql.Timestamp;
+
+/**
+ * Interface for a snapshot.
+ */
+public interface ISnapshot {
+  /**
+   * Get id of snapshot.
+   *
+   * @return String id
+   */
+  public String getId();
+  /**
+   * Get timestamp of snapshot.
+   *
+   * @return Timestamp timestamp
+   */
+  public Timestamp getTimestamp();
+
+  /**
+   * Get description of snapshot.
+   *
+   * @return String description
+   */
+  public String getDescription();
+
+  /**
+   * Get canvas associated with snapshot.
+   *
+   * @return String id
+   */
+  public ICanvas getCanvas();
+  /**
+   * Print out string version of canvas with its values.
+   *
+   * @return String
+   */
+  public String toString();
+}
