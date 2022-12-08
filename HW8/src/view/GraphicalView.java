9bef3eb2083314e5b6acfe56e0027de9982093ce
Yi Zu <zuyi@bu.edu>
2022-12-08 13:05:39 -0500

    Add top, mid, bottom layout

--- HW8/src/view/GraphicalView.java
+++ HW8/src/view/GraphicalView.java
@@ -12,7 +12,9 @@ public class GraphicalView implements ActionListener {
   private static final int HEIGHT = 1000;
 
   // TODO: Get timestamp from controller
-  private JLabel label = new JLabel("Timestamp here");
+  private JLabel tsLabel = new JLabel("Timestamp here");
+
+  private JLabel shapesLabel = new JLabel("TODO: Shapes here");
   private JButton nextBtn = new JButton("Next Snapshot");
 
   private JButton prevBtn = new JButton("Prev Snapshot");
@@ -21,29 +23,45 @@ public class GraphicalView implements ActionListener {
 
   private JButton quitBtn = new JButton("Quit");
   private JFrame frame = new JFrame();
-  private JPanel panel = new JPanel();
+  private JPanel buttonPanel = new JPanel();
+  private JPanel shapesPanel = new JPanel();
+  private JPanel topPanel = new JPanel();
 
   public GraphicalView() {
 
 
     frame.setSize(WIDTH, HEIGHT);
-    panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+    topPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+    shapesPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+    buttonPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 //    panel.setLayout(new GridLayout(10, 10));
 
-    panel.add(nextBtn);
-    panel.add(selectBtn);
-    panel.add(prevBtn);
-    panel.add(quitBtn);
+    // Top panel for timestamp
+    topPanel.add(tsLabel);
+
+    // Center panel for shapes
+    shapesPanel.add(shapesLabel);
+
+    // Button panel
+
+    buttonPanel.add(nextBtn);
+    buttonPanel.add(selectBtn);
+    buttonPanel.add(prevBtn);
+    buttonPanel.add(quitBtn);
 
     nextBtn.addActionListener(this);
 
     // Setup frame
     // 1. Add panel to the frame
-    frame.add(panel, BorderLayout.SOUTH);
+    frame.add(topPanel, BorderLayout.NORTH);
+    frame.add(shapesPanel, BorderLayout.CENTER);
+    frame.add(buttonPanel, BorderLayout.SOUTH);
+
     // 2. Set behavior on frame closing
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     // 3. Set title to window
-    frame.setTitle("Our GUI");
+    frame.setTitle("CS5004 Shapes Photo Album Viewer");
     // 4. Set window to certain size
     frame.setVisible(true);
 
