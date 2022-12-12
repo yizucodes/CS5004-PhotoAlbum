468e3b65d17187daeb65913a3022f0a815337c60
Yi Zu <zuyi@bu.edu>
2022-12-11 22:23:25 -0500

    Upload entry point

--- HW8/src/PhotoAlbumMain.java
+++ HW8/src/PhotoAlbumMain.java
@@ -1,129 +1,101 @@
-//import java.io.IOException;
-//import java.io.InputStreamReader;
-//import java.util.Objects;
-//import java.util.Scanner;
-//
-//import controller.IController;
-//import model.Album;
-//
-//public class PhotoAlbumMain {
-//
-////  MyProgram -in buildings.txt -out myWeb.html  -v web
-////
-////  MyProgram -in buildings.txt -v graphical 800 800 --> passed to the controller
-////
-////  Characteristics of a valid input are:
-////
-////  Each pair of arguments (-in "input-file", -out "output-file", etc.) may appear in any order (e.g. the -view pair can appear first, followed by -in and so on)
-////
-////  Each pair of arguments are ordered. That is, if the user types -in then the next input must be the name of an input file, and so on.
-////
-////  Providing an input file (the -in pair) and a view (the -view pair) are mandatory. If the output set is not specified and the view needs it, gracefully exit the program with an error message indicating why the program did not run..
-//
-//  public static void main(String[] args) {
-//    // You read terminal inputs
-//
-//    // if file
-//
-//    // Create controller
-//
-//    // Create model
-//
-//    // Create view
-//
-//
-//  }
-//}
-//
-//package controller;
-//
-//
-//        import java.io.IOException;
-//        import java.io.InputStreamReader;
-//        import java.rmi.server.ExportException;
-//        import java.util.Objects;
-//        import java.util.Scanner;
-//
-//        import model.Album;
-//
-//public class Controller implements IController {
-//
-//  final Readable in;
-//  final Appendable out;
-//
-//  private static final int DEFAULT_X_HEIGHT = 1000;
-//  private static final int DEFAULT_Y_WIDTH = 1000;
-//
-//  private static final int PAIR = 2;
-//
-//  public Controller(Readable in, Appendable out) {
-//    this.in = in;
-//    this.out = out;
-//  }
-//
-//  /**
-//   * Parse input file.
-//   */
-//  @Override
-//  public void parseFile(Album album) throws IOException {
-//    Objects.requireNonNull(album);
-//    String token = "";
-//    int pairCount = 0;
-//
-//    boolean hasInputFile = false;
-//    boolean hasOutputView = false;
-//
-//    Scanner scan = new Scanner(this.in);
-//
-//    // Continue until terminal cmd is ended
-//    while(scan.hasNext()) {
-//      token = scan.next();
-//
-//      if (token.equalsIgnoreCase("-in")) {
-//
-//        // Check if filename before contains .txt, check if there is a char before .txt
-//        if (scan.next().contains(".txt")) {
-//          // TODO: Read file
-//          System.out.println("Reading text file");
-//          hasInputFile = true;
-//        }
-//
-//      }
-//
-//      // Webview
-//      if (token.equalsIgnoreCase("-out")) {
-//        // Check if filename before contains .html, check if there is a char before .html
-//        if (scan.next().contains(".html")) {
-//          System.out.println("Display web view");
-//          hasOutputView = true;
-//        }
-//
-//      }
-//
-//      // GUI
-//      if (token.equalsIgnoreCase("-v") || token.equalsIgnoreCase("-view") ) {
-//        if (scan.next().equalsIgnoreCase("graphical")) {
-//          System.out.println("Display java swing view");
-//        }
-//      }
-//
-//    }
-//
-//    // If output set not occurring --> error message
-//    if (!(hasInputFile && hasOutputView)) {
-//      throw new IOException("Please provide both input text file and view type.");
-//    }
-//
-//    scan.close();
-//
-//  }
-//
-//  public static void main(String[] args) {
-//    try {
-//      new controller.Controller(new InputStreamReader(System.in), System.out).parseFile(new Album());
-//
-//    } catch (IOException e) {
-//      e.printStackTrace();
-//    }
-//  }
-//}
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import model.Album;
+import controller.Controller;
+
+
+/**
+ * The type Main.
+ */
+public class PhotoAlbumMain {
+
+  public static final int REQUIRED_WEB_VIEW_ARGS = 6;
+  public static final int REQUIRED_GRAPHICAL_VIEW_ARGS = 4;
+  public static final int ERROR_STATUS = 2;
+  public static final int INDEX_DISTANCE_XMAX = 2;
+  public static final int INDEX_DISTANCE_YMAX = 3;
+
+  /**
+   * Entry point of application.
+   *
+   * @param args input arguments from command line.
+   * @throws FileNotFoundException File not found exception.
+   */
+  public static void main(String[] args) throws FileNotFoundException {
+    // Using ArrayList to hold string arguments as terminal command input's size varies
+
+    ArrayList<String> arguments = (ArrayList<String>) Arrays.stream(args).collect(Collectors.toList());
+    File inFile = null;
+    File out = null;
+    String viewType = "";
+    int xMax = -1;
+    int yMax = -1;
+    Album album = new Album();
+
+    if((!arguments.contains("-v") && !arguments.contains("-view") || !arguments.contains("-in"))) {
+      System.exit(ERROR_STATUS);
+    }
+
+    if(arguments.contains("-in")) {
+      int index = 0;
+      for(;index < args.length; index++) {
+        if(arguments.get(index).equalsIgnoreCase("-in")) {
+          break;
+        }
+      }
+      inFile = new File(arguments.get(index + 1));
+    }
+
+    if(arguments.contains("-out")) {
+      int index = 0;
+      for(;index < args.length; index++) {
+        if(arguments.get(index).equalsIgnoreCase("-out")) {
+          break;
+        }
+      }
+      out = new File(arguments.get(index + 1));
+    }
+
+    if(arguments.contains("-v") || arguments.contains("-view")) {
+      int index = 0;
+      for(;index < args.length; index++) {
+        if(arguments.get(index).equalsIgnoreCase("-v") || arguments.get(index).equalsIgnoreCase("-view")) {
+          break;
+        }
+      }
+      int viewInd = index;
+      viewType = arguments.get(index + 1);
+
+      if(viewType.equalsIgnoreCase("web")) {
+        if(!arguments.contains("-out")){
+          System.exit(ERROR_STATUS);
+        }
+        for(;index < args.length; index++) {
+          if (arguments.get(index).equalsIgnoreCase("-out")) {
+            break;
+          }
+
+        }
+        out = new File(arguments.get(index + 1));
+        if(arguments.size() > REQUIRED_WEB_VIEW_ARGS) {
+          // Adding indices to get respected maximum size of window (x and y)
+          xMax = Integer.parseInt(arguments.get(viewInd + INDEX_DISTANCE_XMAX));
+          yMax = Integer.parseInt(arguments.get(viewInd + INDEX_DISTANCE_YMAX));
+        }
+      } else if(viewType.equalsIgnoreCase("graphical")) {
+        if(arguments.size() > REQUIRED_GRAPHICAL_VIEW_ARGS) {
+          // Adding indices to get respected maximum size of window (x and y)
+          xMax = Integer.parseInt(arguments.get(viewInd + INDEX_DISTANCE_XMAX));
+          yMax = Integer.parseInt(arguments.get(viewInd + INDEX_DISTANCE_YMAX));
+        }
+      }
+    }
+
+    new Controller(inFile, xMax, yMax, album, out, viewType).ParseFile();
+
+  }
+}
